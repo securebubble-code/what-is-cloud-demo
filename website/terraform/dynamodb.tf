a5ae3a6edc963d1995ea3ee9138b6346ba411b8c
Antony Grace <antony@securebubble.co.uk>
2023-01-18 12:02:48 +0000

    updated dynamodb entries

--- website/terraform/dynamodb.tf
+++ website/terraform/dynamodb.tf
@@ -18,11 +18,18 @@ resource "aws_dynamodb_table_item" "main-website-table-item-01" {
 
   item = <<ITEM
 {
-  "Data1": {"S": "Interesting"},
-  "one": {"N": "11111"},
-  "two": {"N": "22222"},
-  "three": {"N": "33333"},
-  "four": {"N": "44444"}
+  "Subject": {"S": "antony"},
+  "colour": {"S": "blue"},
+  "sport": {"S": "formula_one"},
+  "animal": {"S": "dogs"},
+  "cloud": {"S": "couldn't possibly say ;)"}
+}
+{
+  "Subject": {"S": "sherlock"},
+  "colour": {"S": "pink"},
+  "sport": {"S": "the game"},
+  "animal": {"S": "hounds"},
+  "cloud": {"S": "the cloud of death"}
 }
 ITEM
 }
